36ffbcbf0c4c31b9423f43f09807bcb332a22aae
Nicolas Marri <nicomarri22@gmail.com>
2021-12-27 18:07:32 -0300

    Creating the new budget component

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const NuevoPresupuesto = () => {
+    return (
+        <div className="contenedor-presupuesto contenedor sombra">
+            
+            <form className="formulario">
+                <div className="campo">
+                    <label>
+                        Definir Presupuesto
+                    </label>
+                    <input
+                        className="nuevo-presupuesto"    
+                        type="text"
+                        placeholder="Añade tu presupuesto"
+                    />
+                </div>
+
+                <input
+                    type="submit"
+                    value="Añadir"
+                />
+            </form>
+        </div>
+    )
+}
+
+export default NuevoPresupuesto
